9122b4f4eb57c7e11ac9e651752eab611ef93e3f
CeniusM <cenius.meier@gmail.com>
2022-02-13 15:44:17 +0100

    changes

--- Chess/Move/PieceMovment/Rook.cs
+++ Chess/Move/PieceMovment/Rook.cs
@@ -7,11 +7,19 @@ namespace Chess.Moves.PieceMovment
     {
         public static bool IsMovePoseble(Board board, PosebleMoves.Move move)
         {
+            int[] directionValues =
+            {
+                -8, // north
+                8, // south
+                1, // east
+                -1, // west
+            };
+
             bool IsMovePoseble = false;
 
             for (int i = 0; i < 4; i++)
             {
-
+                
             }
 
             return IsMovePoseble;
