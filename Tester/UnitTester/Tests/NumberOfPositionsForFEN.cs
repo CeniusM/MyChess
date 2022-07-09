7e1dbe4d920bde1e90acb5937852eb449a2e1f35
CeniusM <cenius.meier@gmail.com>
2022-07-09 22:01:22 +0200

    changes

--- Tester/UnitTester/Tests/NumberOfPositionsForFEN.cs
+++ Tester/UnitTester/Tests/NumberOfPositionsForFEN.cs
@@ -26,74 +26,83 @@ namespace MyChess.UnitTester.Tests
             // long[] foo3 = { 14, 191, 2812, 43238, 674624, 11030083, 178633661, 3009794393 };
             // CS_MyConsole.MyConsole.WriteLine(FullSearchPrint("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", foo3, 6));
 
-            CS_MyConsole.MyConsole.WriteLine("Standerd Pos: Initial Position");
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 20, 1));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 400, 2));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 8902, 3));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 197281, 4));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 4865609, 5));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 119060324, 6));
-            /*CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 3195901860, 7));*/
+            // CS_MyConsole.MyConsole.WriteLine("Standerd Pos: Initial Position");
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 20, 1));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 400, 2));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 8902, 3));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 197281, 4));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 4865609, 5));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 119060324, 6));
+            // /*CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 3195901860, 7));*/
 
 
-            CS_MyConsole.MyConsole.WriteLine("");
-            CS_MyConsole.MyConsole.WriteLine("Test Casteling: Position 2");
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 48, 1));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 2039, 2));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 97862, 3));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 4085603, 4));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 193690690, 5));
-            /*CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 8031647685, 6));*/
+            // CS_MyConsole.MyConsole.WriteLine("");
+            // CS_MyConsole.MyConsole.WriteLine("Test Casteling: Position 2");
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 48, 1));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 2039, 2));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 97862, 3));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 4085603, 4));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 193690690, 5));
+            // /*CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 8031647685, 6));*/
 
 
-            CS_MyConsole.MyConsole.WriteLine("");
-            CS_MyConsole.MyConsole.WriteLine("Test Pawn Rook Endgame: Position 3");
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 14, 1));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 191, 2));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 2812, 3));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 43238, 4));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 674624, 5));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 11030083, 6));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 178633661, 7));
-            /*CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 3009794393, 8));*/
+            // CS_MyConsole.MyConsole.WriteLine("");
+            // CS_MyConsole.MyConsole.WriteLine("Test Pawn Rook Endgame: Position 3");
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 14, 1));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 191, 2));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 2812, 3));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 43238, 4));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 674624, 5));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 11030083, 6));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 178633661, 7));
+            // /*CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 3009794393, 8));*/
 
 
-            CS_MyConsole.MyConsole.WriteLine("");
-            CS_MyConsole.MyConsole.WriteLine("Test White side Casteling: Position 4");
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 6, 1));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 264, 2));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 9467, 3));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 422333, 4));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 15833292, 5));
-            /*CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 706045033, 6));*/
+            // CS_MyConsole.MyConsole.WriteLine("");
+            // CS_MyConsole.MyConsole.WriteLine("Test White side Casteling: Position 4");
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 6, 1));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 264, 2));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 9467, 3));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 422333, 4));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 15833292, 5));
+            // /*CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 706045033, 6));*/
 
 
-            CS_MyConsole.MyConsole.WriteLine("");
-            CS_MyConsole.MyConsole.WriteLine("Test Black side Casteling: Position 4");
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 6, 1));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 264, 2));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 9467, 3));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 422333, 4));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 15833292, 5));
-            /*CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 706045033, 6));*/
+            // CS_MyConsole.MyConsole.WriteLine("");
+            // CS_MyConsole.MyConsole.WriteLine("Test Black side Casteling: Position 4");
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 6, 1));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 264, 2));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 9467, 3));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 422333, 4));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 15833292, 5));
+            // /*CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 706045033, 6));*/
 
 
-            CS_MyConsole.MyConsole.WriteLine("");
-            CS_MyConsole.MyConsole.WriteLine("Perft Position 5 game");
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 44, 1));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 1486, 2));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 62379, 3));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 2103487, 4));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 89941194, 5));
+            // CS_MyConsole.MyConsole.WriteLine("");
+            // CS_MyConsole.MyConsole.WriteLine("Perft Position 5 game");
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 44, 1));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 1486, 2));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 62379, 3));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 2103487, 4));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 89941194, 5));
 
 
-            CS_MyConsole.MyConsole.WriteLine("");
-            CS_MyConsole.MyConsole.WriteLine("Perft Position 6 game");
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 46, 1));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 2079, 2));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 89890, 3));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 3894594, 4));
-            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 164075551, 5));
+            // CS_MyConsole.MyConsole.WriteLine("");
+            // CS_MyConsole.MyConsole.WriteLine("Perft Position 6 game");
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 46, 1));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 2079, 2));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 89890, 3));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 3894594, 4));
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 164075551, 5));
+        
+            
+        
+        
+        
+            // CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/8/6k1/8/8/2K5/8/8 w - - 0 1", 0, 8));
+            CS_MyConsole.MyConsole.WriteLine(PerftResultSearch(false, "8/8/6k1/8/8/2K5/8/8 w - - 0 1", 54948733, 9));
+        
+        
         }
 
         private static string PerftResultSearch(bool PrintStats, string FEN, long ExpectedValue, int Depth)
